ee4ee1d80abdaa10061fcdc4f7b35fe942c6d920
Divy Gobiraj <divygobi@gmail.com>
2023-10-17 15:28:50 +0530

    1.2 Hello World from rust docs

---
+++ hello_world/main.rs
@@ -0,0 +1,5 @@
+fn main(){
+    //this is a comment
+    //println! is a macro that prints text to the console, note the ! in front of println, this means its a macro and not a function
+    println!("Hello, world!");
+}
